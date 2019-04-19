a2531aacd2abd118e1de26cd21390842d955f252
fyssunwenbo <fyssunwenbo@gmail.com>
2019-04-19 02:38:08 +0200

    version1 completo

---
+++ version1/conjunto.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+
+using namespace std;
+const int DIMENSION = 9;
+typedef int tElementos[DIMENSION]; //Tipo de datos que representa
+//un subconjunto del conjunto de elementos{ 1,2,3,4,5,6,7,8,9 }
+
+typedef struct {
+	int nElems; //cardinal del conjunto
+	tElementos elementos; //elementos contenidos en el conjunto
+} tConjunto;
+
+void cjto_vacio(tConjunto &c);   // inicializa el conjunto c al conjunto vacío.
+void cjto_lleno(tConjunto &c); // inicializa el conjunto c al conjunto formado por todos los valores del intervalo[1, 9].
+bool pertenece(const tConjunto &c, int e);  // devuelve un booleano que indica si el elemento e(nº entero [1, 9]) se encuentra en el conjunto c.
+void annadeElemento(tConjunto &c, int e);  // mete el elemento e (nº entero [1, 9]) en el conjunto c.
+void borraElemento(tConjunto &c, int e);  // saca el elemento e (nº entero [1, 9]) del conjunto c.
+int numElems(const tConjunto &c);   // devuelve el nº de elementos que hay en el conjunto c.
+bool esUnitario(const tConjunto &c, int &e);   // devuelve un booleano que indica si c tiene un único elemento y, de ser así, lo devuelve.
+void mostrar(const tConjunto &c); // visualiza por pantalla los elementos del conjunto c.
---
+++ version1/listaSudokus.cpp
@@ -0,0 +1,54 @@
+#include "listaSudokus.h"
+
+
+void creaListaVacia(tListaSudokus &lista) {
+	for (int i = 0; i < MAX_SUDOKUS; i++) {
+		lista.array[i].fichero = " ";
+	}
+	lista.cont = 0;
+}
+
+bool cargar(tListaSudokus &lista) {
+	bool ok = false;
+	ifstream inputFile;
+	string  listaSudokus = "listaSudokus.txt";
+	inputFile.open(listaSudokus);
+	if (!inputFile.is_open()) {
+		cout << "ERROR:No se ha podido abrir el archivo: " << listaSudokus << endl;
+	}
+	else {
+		creaListaVacia(lista);
+		int i = 0;
+		while ((!inputFile.eof())&&i<MAX_SUDOKUS) {
+				inputFile >> lista.array[i].fichero >> lista.array[i].nivel;
+				lista.cont++;
+				i++;	
+		}
+		ok = true;
+	}
+	return ok;
+}
+
+void mostrar(const tListaSudokus &lista) {
+	cout << "     NOMBRE       NIVEL" << endl;
+	for (int i = 0; i < lista.cont-1; ++i) {
+		cout << i + 1 << "." << lista.array[i].fichero << right << setw(4) << lista.array[i].nivel << endl;
+	}
+}
+
+int menuListaSudokus(const tListaSudokus &lista) {
+	int op; int points;
+	mostrar(lista);
+	cout << "Choose a Sudoku Game: " << endl;
+	cin >> op;
+	while (op > lista.cont||op<0) {
+		cout << "Choose a Sudoku Game: " << endl;
+		cin >> op;
+	}
+	if (op <= lista.cont ) {
+		points=jugarUnSudoku(lista.array[op-1]);
+		
+	}
+
+	return points;
+}
